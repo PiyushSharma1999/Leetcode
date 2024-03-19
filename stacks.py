c763da0fe21665dab71f2e8b37bd6c015dd39dfb
Piyush Sharma <piyush316692@gmail.com>
2024-03-20 01:39:08 +0530

    baseball game solved

--- stacks.py
+++ stacks.py
@@ -1,19 +1,20 @@
 ops = ["5","-2","4","C","D","9","+","+"]
 stack = []
 res = 0
+temp = 0
 for i in ops:
-    if i.isnumeric() or i.match("/^-\d+$/"):
-        stack.append(int(i))
-    elif i == "C" and len(stack) != 0:
+    if i == "C" and len(stack) != 0:
         stack.pop()
     elif i == "D" and len(stack) != 0:
         stack.append(stack[-1]*2)
-        res += stack[-1]
+        
     elif i == "+" and len(stack) >=2:
         stack.append(stack[-1]+stack[-2])
-        res += stack[-1]
+        
+    else:
+        stack.append(int(i))
 
 for i in stack:
     res+=i
 
-print(("-2").isnumeric())
+print(res)
